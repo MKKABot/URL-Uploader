bb9117ec4f7acf342952f2ed654dd30a7fea0584
MKKABot <107781886+MKKABot@users.noreply.github.com>
2022-06-28 09:39:14 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Tellybots/Uploader-Bot /Uploader-Bot
-cd /Uploader-Bot
+git clone https://github.com/MKKABot/URL-Uploader /URL-Uploader
+cd /URL-Uploader
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
+echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @MKKA\n Special Credit :- @MKKA"
 python3 bot.py
